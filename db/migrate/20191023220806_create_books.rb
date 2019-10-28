d7be1a4647c873d413b84c80a0a2973d3e29cb61
Aaron <aaron-tt@hotmail.com>
2019-10-28 14:27:45 -0500

    Fix the migration table by adding foreign keys Added bootstrap and jquery-rails in gemfile

--- db/migrate/20191023220806_create_books.rb
+++ db/migrate/20191023220806_create_books.rb
@@ -3,10 +3,10 @@ class CreateBooks < ActiveRecord::Migration[6.0]
     create_table :books do |t|
       t.integer :book_id
       t.string :title
-      t.integer :author_id
-      t.integer :publisher_id
-      t.integer :category_id
-      t.integer :superhero_id
+      t.references :author, null: false, foreign_key: true
+      t.references :publisher, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+      t.references :superhero, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20191023221505_create_superheros.rb
+++ db/migrate/20191023221505_create_superheros.rb
@@ -3,6 +3,7 @@ class CreateSuperheros < ActiveRecord::Migration[6.0]
     create_table :superheros do |t|
       t.integer :superhero_id
       t.string :superhero_name
+      t.references :superpower, null: false, foreign_key: true
 
       t.timestamps
     end

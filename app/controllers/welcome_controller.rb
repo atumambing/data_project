5c57c88e4673015587c5f0886b722475c503e720
Aaron <aaron-tt@hotmail.com>
2019-11-04 08:54:44 -0600

    Attempting to use CSV file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,9 @@ class WelcomeController < ApplicationController
   http_basic_authenticate_with name: "username", password: "password"
 
   def index
-    @titles = Book.all
+    @books = Book.all
   end
 
   def show
-    @categories = Category.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,32 @@
-require 'faker
-include 'Faker'
+require 'faker'
+require 'csv'
 
-5.times do
+CSV.foreach(Rails.root.join('lib/heroes_csv.csv'), headers: true) do |row|
+  Book.create({
+    title: row[5]
+    })
+  end
+
+1.times do
   Category.create(
-    category_name: Faker::Book.unique.genre
+    category_name: Faker::Book.genre
   )
 end
 
-#1.times do
-#  Superpower.create(
-#    superpower_name: Faker::Superhero.unique.power
-#  )
-#end
+1.times do
+  Superpower.create(
+    superpower_name: Faker::Superhero.power
+  )
+end
 
-#1.times do
-#  Superhero.create(
-#    superhero_name: Faker::DcComics.unique.hero
-#  )
-#end
-
-#1.times do
-#  Superhero.create(
-#    superhero_name: Faker::DcComics.unique.heroine
-#  )
-#end
-
-5.times do
+1.times do
   Publisher.create(
-    publisher_name: Faker::Book.unique.publisher
+    publisher_name: Faker::Book.publisher
   )
 end
 
-5.times do
+1.times do
   Author.create(
-    author_name: Faker::Book.unique.author
-  )
-end
-
-15.times do
-  Book.create(
-    title: Faker::DcComics.unique.title
+    author_name: Faker::Book.author
   )
 end
